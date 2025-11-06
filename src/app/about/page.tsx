6a26da438f4ce88a1002c5b1b3a6ad3f66de64d4
miknivin <116654570+miknivin@users.noreply.github.com>
2025-11-06 11:49:41 +0530

    content changes

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,14 +2,14 @@ import About from "@/components/about";
 import Wrapper from "@/layouts/Wrapper";
 
 export const metadata = {
-   title: "About iw3",
+  title: "About iw3",
 };
 const page = () => {
-   return (
-      <Wrapper>
-         <About />
-      </Wrapper>
-   )
-}
+  return (
+    <Wrapper>
+      <About />
+    </Wrapper>
+  );
+};
 
-export default page
+export default page;
